b2f712fa8024174a09e5508694c7b4f65f517929
Code Sandbox <hello@codesandbox.io>
2022-04-13 05:53:36 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.aac7faf08e751dcb47685fbb46f357a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b68d81b7dc8a4857d295",
+    "url": "https://harryji168.github.io/csb-u4lf1p/static/js/main.e53ceaee.chunk.js"
+  },
+  {
+    "revision": "e31614b41c5356d75af8",
+    "url": "https://harryji168.github.io/csb-u4lf1p/static/js/runtime~main.89e73e83.js"
+  },
+  {
+    "revision": "1a655a4291a534c83d86",
+    "url": "https://harryji168.github.io/csb-u4lf1p/static/css/2.33f864ee.chunk.css"
+  },
+  {
+    "revision": "1a655a4291a534c83d86",
+    "url": "https://harryji168.github.io/csb-u4lf1p/static/js/2.1bbe5090.chunk.js"
+  },
+  {
+    "revision": "84b5d6c07d5c61d22b2e5fcd6659ed0d",
+    "url": "https://harryji168.github.io/csb-u4lf1p/index.html"
+  }
+];
